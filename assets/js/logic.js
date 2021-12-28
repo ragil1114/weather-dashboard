975d18257feb9d97cb7fc9c5bb286186ccc653a3
Ragil14 <rahil.gilani@gmail.com>
2021-12-28 17:51:08 -0600

    added forecastTitle & forecastContainerEl variables using query selectors

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,6 +3,8 @@ var cityFormEl=document.querySelector("#city-search-form");
 var cityInputEl=document.querySelector("#city");
 var weatherContainerEl=document.querySelector("#current-weather-container");
 var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
 
 
 var formSumbitHandler = function(event) {
