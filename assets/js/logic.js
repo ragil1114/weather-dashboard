106337e3566e11f5bb9328b95c64d2387d873e19
Ragil14 <rahil.gilani@gmail.com>
2021-12-28 18:48:34 -0600

    added pastSearch() function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -176,4 +176,16 @@ var display5Day = function(weather) {
 }
 
 
+var pastSearch = function(pastSearch) {
+
+    pastSearchEl = document.createElement("button");
+    pastSearchEl.textContent = pastSearch;
+    pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+    pastSearchEl.setAttribute("data-city",pastSearch)
+    pastSearchEl.setAttribute("type", "submit");
+
+    pastSearchButtonEl.prepend(pastSearchEl);
+}
+
+
 cityFormEl.addEventListener("submit", formSumbitHandler);
